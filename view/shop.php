3ef13a4f290cb234c1eb2dfdead2702f41107afc
Gustavo Nascimento <o_fantasma@hotmail.com.br>
2019-10-25 19:14:36 -0300

    Adicinando os valores

--- view/shop.php
+++ view/shop.php
@@ -2,28 +2,27 @@
 
 <section>
 
-    <div class="container">                
+    <div class="container" id="destaque-produtos-container">                
 			
         <div id="destaque-produtos">
             
             <div class="item">
 
-                <div class="col-sm-6">
+                <div class="col-sm-6 col-imagem">
                     <img src="img/produtos/moto-x.png" alt="Nome do produtos">                  
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-6 col-descricao">
                     <h2>Smartphone Motorola Moto X Play Dual Chip Desbloqueado Android 5.1</h2>                  
                     <div class="box-valor">
-                        <div class="text-noboleto">no boleto</div>
+                        <div class="text-noboleto text-arial-cinza">no boleto</div>
                         <div class="text-por">por</div>
-                        <div class="text-reias">R$</div>
-                        <div class="text-valor">1.259</div>
-                        <div class="text-valor-centavos">,10</div>
-                        <div class="text-parcelas">ou em até 8x de 174,88</div>
-                        <div class="text-total">total a prazo R$ 1.399,00</div>
-                        <a href="#" class="btn btn-comprar"><i class="fa fa-shopping-cart"></i>compre agora</a>
+                        <div class="text-reias text-roxo">R$</div>
+                        <div class="text-valor text-roxo">1.259</div>
+                        <div class="text-valor-centavos text-roxo">,10</div>
+                        <div class="text-parcelas text-arial-cinza">ou em até 8x de 174,88</div>
+                        <div class="text-total text-arial-cinza">total a prazo R$ 1.399,00</div>
                     </div>           
-
+                    <a href="#" class="btn btn-comprar text-roxo"><i class="fa fa-shopping-cart"></i>compre agora</a>
                 </div>
             
             </div>
